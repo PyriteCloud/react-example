f2a697205f6e809c3ff91f91bfffc45d41cb3d81
AyushChothe <40957514+AyushChothe@users.noreply.github.com>
2025-03-19 20:37:22 +0530

    Updated Welcome message

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -6,7 +6,7 @@ export function Welcome() {
     <main className="flex items-center justify-center pt-16 pb-4">
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
         <header className="flex flex-col items-center gap-9">
-         <h1>Welcome to Pyrite Cloud V1</h1>
+         <h1>Welcome to Pyrite Cloud V2</h1>
           <div className="w-[500px] max-w-[100vw] p-4">
             <img
               src={logoLight}
